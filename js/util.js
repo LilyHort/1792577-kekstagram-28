a91d12914e1912021cf119b47286749ddc4d8c82
Лилиана Леднёва <lilyledneva@gmail.com>
2023-04-09 17:05:47 +0300

    Добавляет фильтры

--- js/util.js
+++ js/util.js
@@ -13,3 +13,25 @@ export const request = async (url, options) => {
 
   return response.json();
 };
+
+/**
+ * @param {(...args: any) => any} callback
+ * @param {number} delay
+ * @return {(...args: any) => any}
+ */
+export const debounce = (callback, delay = 500) => {
+  let timeoutId;
+  let lastCallTime;
+
+  return (...rest) => {
+    const elapsedTime = Date.now() - lastCallTime;
+    const newDelay = Math.max(delay - elapsedTime, 0);
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      callback(...rest);
+      lastCallTime = Date.now();
+    }, newDelay);
+  };
+};
